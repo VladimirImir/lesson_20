17abd763ad65f089b2db6dbf480dcf7cb9ce5094
VladimirImir <mimimvvv@gmail.com>
2023-06-10 16:22:02 +0300

    21.2 - Stream.

---
+++ src/com/dev/lesson21/StreamExample.java
@@ -0,0 +1,33 @@
+package com.dev.lesson21;
+
+import java.util.List;
+
+/**
+ * Stream.
+ */
+
+public class StreamExample {
+
+    public static void main(String[] args) {
+        List<String> strings = List.of("88", "11", "22", "33", "44", "55", "66");
+
+        /** Вариант написания c Stream. */
+        strings.stream()
+                .map(string -> string + string)
+                .map(Integer::valueOf)
+                .filter(value -> value % 2 == 0)
+                .sorted()
+                .skip(1)
+                .limit(2)
+                .forEach(System.out::println);
+
+        /** Вариант написания без Stream. */
+        /*for (String string : strings) {
+            String value = string + string;
+            Integer intValue = Integer.valueOf(value);
+            if (intValue % 2 == 0) {
+                System.out.println(intValue);
+            }
+        }*/
+    }
+}
