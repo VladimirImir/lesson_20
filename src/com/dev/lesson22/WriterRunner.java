30e910561a81fe56c5dd35b92ecf5737b7e0ffdb
VladimirImir <mimimvvv@gmail.com>
2023-06-11 21:56:55 +0300

    22.6 - Класс Files.

--- src/com/dev/lesson22/WriterRunner.java
+++ src/com/dev/lesson22/WriterRunner.java
@@ -4,17 +4,31 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 public class WriterRunner {
 
     public static void main(String[] args) throws IOException {
-        File file = Path.of("resources", "writer.txt").toFile();
+        /*File file = Path.of("resources", "writer.txt").toFile();
         try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(file, true))) {
             fileWriter.newLine();
             fileWriter.append("Hello World!");
             fileWriter.newLine();
             fileWriter.append("Java");
-        }
+        }*/
+
+        /** Класс - Files. */
+
+        Path path = Path.of("resources", "writer2.txt");
+        Files.write(path, List.of("Hello world!", "Java"));
+
+        /*try (BufferedWriter fileWriter = Files.newBufferedWriter(file)) {
+            fileWriter.newLine();
+            fileWriter.append("Hello World!");
+            fileWriter.newLine();
+            fileWriter.append("Java");
+        }*/
     }
 }
